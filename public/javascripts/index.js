e4a1a12fcae77ad098c662c06db4ec8adf20c2bf
yesukhei <yesukhei04@gmail.com>
2024-08-14 18:42:58 +0800

    8-14

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,34 +1,89 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  
+  
     const urlParams = new URLSearchParams(window.location.search);
     const subId = urlParams.get("subId");
     console.log("subID: ", subId);
 
-    const superGifts = [
-
-        {name:"charming", img : "../images/gifts/CHARMING_01.png"},
-        {name:"air", img : "../images/gifts/AIR_01.png"},
-        {name:"delonghi", img : "../images/gifts/Delonghi_01.png"},
-        {name:"dualipa", img : "../images/gifts/DUALIPA_01.png"},
-        {name:"macbook", img : "../images/gifts/MACBOOK_01.png"},
-        {name:"steamdeck", img : "../images/gifts/STEAMDECK_01.png"},
-    ];
-    
     var isStartPage = true;
 
     var balance = 100;
 
+    const superGifts = [
+        {name:"charming", img : "../images/gifts/CHARMING_01.png", price:10},
+        {name:"air", img : "../images/gifts/AIR_01.png", price:10},
+        {name:"delonghi", img : "../images/gifts/Delonghi_01.png", price:10},
+        {name:"dualipa", img : "../images/gifts/DUALIPA_01.png", price:10},
+        {name:"macbook", img : "../images/gifts/MACBOOK_01.png", price:10},
+        {name:"steamdeck", img : "../images/gifts/STEAMDECK_01.png", price:10},
+
+    ];
+
+    const vouchers = [
+        {name:"apple", img : "../images/vouchers/apple.png", price : 60},
+        {name:"chinese", img : "../images/vouchers/chinese.png", price : 40},
+        {name:"entertainment", img : "../images/vouchers/entertainment.png", price : 10},
+        {name:"karaoke", img : "../images/vouchers/karaoke.png", price : 20},
+        {name:"looktv", img : "../images/vouchers/looktv.png", price : 40},
+        {name:"mlbb_diamond", img : "../images/vouchers/mlbb_diamond.png", price : 60},
+        {name:"mlbb_ticket", img : "../images/vouchers/mlbb_ticket.png", price : 30},
+        {name:"roblox", img : "../images/vouchers/roblox.png", price : 60},
+        {name:"russia", img : "../images/vouchers/russia.png", price : 40},
+        {name:"shoppy", img : "../images/vouchers/shoppy.png", price : 60},
+        {name:"steam", img : "../images/vouchers/steam.png", price : 60},
+        {name:"toki", img : "../images/vouchers/toki.png", price : 60},
+        {name:"tomyo", img : "../images/vouchers/tomyo.png", price : 40},
+    ]
     
+    const cardContainer = document.getElementById("cardContainer");
+    cardContainer.innerHTML = superGifts
+    .map(({name,img,price},i) => 
+    `
+    <div class="card" name = "horCard">
+                <img id="img${i}" src= ${img}
+                    style="height: 250px;  object-fit: contain; border-radius: var(--borderRadius);"
+                    alt=""
+                />
+                <div style="position: absolute; bottom: 0; right: 0; margin: 10px; border-radius: var(--iconBorderRadius); background-color: rgba(43, 37, 37, 0.65);">
+                <div style="display: flex; align-items: center; padding: 5px; gap: 5px">
 
-//     const cardContainer = document.getElementById("cardContainer");
-//     cardContainer.innerHTML = superGifts
-//   .map(({name,img},i) => 
-//     `
-   
-//   `
+                  <h4 style="margin: 0; font-weight: 900; position: relative;">
+                    <span style="position: absolute; right: 0;">
+                      1 ШИРХЭГ
+                    </span>
+                    <br>ТОХИРЛЫН ЭРХ
+                  </h4>
+                  <h2 style="margin: 0; padding-left: 5px; border-left: 3px solid white; font-weight: 900;">
+                    ${price}
+                  </h2>
+                  <img src="../images/star.png"  style="height: 20px; aspect-ratio: 1; object-fit: fit"/>
+                </div>
+                </div>
+              </div>
+  `).join("");
 
-// )
-  
-//   .join("");
+  const vertCardContainer = document.getElementById("vertCardContainer");
+  vertCardContainer.innerHTML = vouchers
+    .map(({name,img,price},i) => 
+    `
+      <div class="card" name = "verCard">
+                  <img src= ${img}
+                      style="height: 200px;  object-fit: contain; border-radius: var(--borderRadius);"
+                      alt=""
+                  />
+                  <div style="position: absolute; bottom: 0; right: 0; margin: 10px; border-radius: var(--iconBorderRadius); background-color: rgba(43, 37, 37, 0.65);">
+                    <div style="display: flex; align-items: center; padding: 5px; gap: 4px;">    
+                      <h2 style="margin: 0; font-weight: 900;">
+                        ${price}
+                      </h2>
+                      <img src="../images/star.png"  style="height: 20px; aspect-ratio: 1; object-fit: fit"/>
+                    </div>
+                    </div>
+                </div>
+  `
+
+).join("");
+
 
 
     
@@ -36,6 +91,23 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.getElementById("startPage").style.display = "none";
     document.getElementById("homePage").style.display = "flex";
     document.getElementById("stars").innerHTML = balance;
+    var tabIndex = 0; 
+
+    // !Tab Index
+    const horElmntsWithTabindex = document.getElementById(`img${tabIndex}`);
+    const verElmntsWithTabindex = document.getElementsByName("verCard");
+
+    // !Indexes
+    const colIndex = 0;
+
+  
+    horElmntsWithTabindex.style.border = "4px, solid, #f0bd1f";
+    horElmntsWithTabindex.style.borderRadius = "20px"; 
+    horElmntsWithTabindex.style.scale= 1.05;
+
+    console.log(horElmntsWithTabindex);
+
+   
     
     
     document.addEventListener("keydown", async (event) => {
@@ -51,73 +123,60 @@ document.addEventListener("DOMContentLoaded", async () => {
                      document.getElementById("homePage").style.display = "flex";
                      isStartPage = false;
                 }
-            //   if (activeModal != "") {
-            //     location.reload();
-            //   }
-            //   if (userInventory.total_voucher == 0) {
-            //     drawSendModalError();
-            //     activeModal = "sendModalError";
-            //     return;
-            //   }
-            //   const carStart = document.getElementById("carStart");
-            //   carStart.style.display = "none";
-            //   car.style.display = "block";
-            //   if (path.length === 0) {
-            //     path = movements.movements;
-            //     carMovement(path);
-            //   } else if (path == movements.up.movements && direction == "") {
-            //     path = movements.up.straight.movements;
-            //     userMoves.push("right");
-            //     carMovement(path);
-            //   } else if (path == movements.down.movements && direction == "") {
-            //     path = movements.down.straight.movements;
-            //     userMoves.push("right");
-            //     carMovement(path);
-            //   } else if (direction === "up") {
-            //     drawGifts();
-            //     userMoves.push("up");
-            //     if (path == movements.up.movements) {
-            //       path = movements.up.up.movements;
-            //     } else if (path == movements.down.movements) {
-            //       path = movements.down.up.movements;
-            //     } else {
-            //       path = movements.up.movements;
-            //     }
-            //     carMovement(path);
-            //   } else if (direction === "down") {
-            //     drawGifts();
-            //     userMoves.push("down");
-            //     if (path == movements.down.movements) {
-            //       path = movements.down.down.movements;
-            //     } else if (path == movements.up.movements) {
-            //       path = movements.up.down.movements;
-            //     } else {
-            //       path = movements.down.movements;
-            //     }
-            //     carMovement(path);
-            //   }
-              break;
-    
-            case "ArrowUp":
-            //   direction = "up";
-            //   arrow1.style.opacity = "100%";
-            //   arrow2.style.opacity = "20%";
-            //   arrow3.style.opacity = "20%";
-              break;
-    
-            case "ArrowDown":
-            //   direction = "down";
-            //   arrow1.style.opacity = "20%";
-            //   arrow3.style.opacity = "100%";
-            //   arrow2.style.opacity = "20%";
-              break;
-    
-            case "ArrowRight":
-            //   direction = "";
-            //   arrow1.style.opacity = "20%";
-            //   arrow3.style.opacity = "20%";
-            //   arrow2.style.opacity = "100%";
-              break;
+             case "ArrowUp" :
+
+             break;
+
+             case "ArrowDown" :
+
+             break;
+
+             case "ArrowRight" :
+                  
+                  
+                  
+                  // document.activeElement.tabIndex;
+                 
+                  
+                  if (tabIndex < horElmntsWithTabindex.length-1) {
+                    tabIndex++;
+
+                    
+                    horElmntsWithTabindex[tabIndex-1].childNodes[1].style.border = "none";
+                    // horElmntsWithTabindex[tabIndex-1].childNodes[1].style.borderRadius = "20px";
+                    horElmntsWithTabindex[tabIndex-1].childNodes[1].style.scale= 1;  
+
+                    horElmntsWithTabindex[tabIndex].childNodes[1].style.border = "4px, solid, #f0bd1f";
+                    horElmntsWithTabindex[tabIndex].childNodes[1].style.borderRadius = "20px"; 
+                    horElmntsWithTabindex[tabIndex].childNodes[1].style.scale= 1.05;
+                  }
+                  // arrayOfElmnts[0].focus()
+                  console.log("tab index :", tabIndex);
+                 
+                  break;
+
+             case "ArrowLeft":
+
+              // var tabIndex = document.activeElement.tabIndex;
+                
+              if (tabIndex > 0) {
+                tabIndex--;
+                horElmntsWithTabindex[tabIndex+1].childNodes[1].style.border = "none";
+                // horElmntsWithTabindex[tabIndex+1].childNodes[1].style.borderRadius = "20px"; 
+                horElmntsWithTabindex[tabIndex+1].childNodes[1].style.scale= 1;
+
+                horElmntsWithTabindex[tabIndex].childNodes[1].style.border = "4px, solid, #f0bd1f";
+                horElmntsWithTabindex[tabIndex].childNodes[1].style.borderRadius = "20px"; 
+                horElmntsWithTabindex[tabIndex].childNodes[1].style.scale= 1.05;
+            
+              }
+            // arrayOfElmnts[0].focus()
+            console.log("tab index :", tabIndex);
+
+                  
+
+             break;
+
           }
         
       });
