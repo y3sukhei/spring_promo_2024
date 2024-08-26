76b339e3037c4831be2fb47ae61f12ef4a07c616
yesukhei <yesukhei04@gmail.com>
2024-08-26 19:06:23 +0800

    8-26

--- public/javascripts/gifts.js
+++ public/javascripts/gifts.js
@@ -26,22 +26,24 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     ];
     const vouchers = [
-        {name:"Apple Gift Card", img : "../images/vouchers/apple.png", price : 60, description:"5$ Gift Card"},
-        {name:"Хятад Багц", img : "../images/vouchers/chinese.png", price : 40, description :'Хятад Багц'},
-        {name:"Энтертайнмент Багц", img : "../images/vouchers/entertainment.png", price : 10, description:'Энтертайнмент Багц'},
-        {name:"karaoke", img : "../images/vouchers/karaoke.png", price : 20, description:'Караоке'},
-        {name:"looktv", img : "../images/vouchers/looktv.png", price : 40,description:'look tv'},
-        {name:"mlbb_diamond", img : "../images/vouchers/mlbb_diamond.png", price : 60, description:'mlbb'},
-        {name:"mlbb_ticket", img : "../images/vouchers/mlbb_ticket.png", price : 30, description:'mlbb'},
-        {name:"roblox", img : "../images/vouchers/roblox.png", price : 60, description:'roblox'},
-        {name:"russia", img : "../images/vouchers/russia.png", price : 40, description:'russia'},
-        {name:"shoppy", img : "../images/vouchers/shoppy.png", price : 60, description:'shoppy'},
-        {name:"steam", img : "../images/vouchers/steam.png", price : 60, description:'steam'},
-        {name:"toki", img : "../images/vouchers/toki.png", price : 60, description:'toki'},
-        {name:"tomyo", img : "../images/vouchers/tomyo.png", price : 40, description:'tomyo'},
+        {name:"Apple Gift Card", img : "../images/vouchers/apple.png", imgLand: "../images/voucher_land/apple.png", price : 60, description:"5$ Gift Card"},
+        {name:"Хятад Багц", img : "../images/vouchers/chinese.png",  imgLand: "../images/voucher_land/chinese.png",price : 40, description :'Хятад Багц'},
+        {name:"Энтертайнмент Багц", img : "../images/vouchers/entertainment.png", imgLand: "../images/voucher_land/entertainment.png", price : 10, description:'Энтертайнмент Багц'},
+        {name:"karaoke", img : "../images/vouchers/karaoke.png", imgLand: "../images/voucher_land/karaoke.png",price : 20, description:'Караоке'},
+        {name:"looktv", img : "../images/vouchers/looktv.png", imgLand: "../images/voucher_land/looktv.png",price : 40,description:'look tv'},
+        {name:"mlbb_diamond", img : "../images/vouchers/mlbb_diamond.png", imgLand: "../images/voucher_land/mlbb_diamond.png", price : 60, description:'mlbb'},
+        {name:"mlbb_ticket", img : "../images/vouchers/mlbb_ticket.png", imgLand: "../images/voucher_land/mlbb_ticket.png", price : 30, description:'mlbb'},
+        {name:"roblox", img : "../images/vouchers/roblox.png", imgLand: "../images/voucher_land/roblox.png", price : 60, description:'roblox'},
+        {name:"russia", img : "../images/vouchers/russia.png", imgLand: "../images/voucher_land/russia.png", price : 40, description:'russia'},
+        {name:"shoppy", img : "../images/vouchers/shoppy.png", imgLand: "../images/voucher_land/shoppy.png", price : 60, description:'shoppy'},
+        {name:"steam", img : "../images/vouchers/steam.png", imgLand: "../images/voucher_land/steam.png", price : 60, description:'steam'},
+        {name:"toki", img : "../images/vouchers/toki.png", imgLand: "../images/voucher_land/toki.png", price : 60, description:'toki'},
+        {name:"tomyo", img : "../images/vouchers/tomyo.png", imgLand: "../images/voucher_land/tomyo.png", price : 40, description:'tomyo'},
     ];
    
     const giftContainer = document.getElementById("giftContainer");
+    const voucherContainer = document.getElementById("voucherContainer");
+
     giftContainer.innerHTML = superGifts
     .map(({name,img,price,description},i) => 
     `
@@ -68,6 +70,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     var tabIndex = 0;
     var colIndex = 0;
+    var voucherColIndex = 0;
     var isAnimating = false;
 
     document.getElementById(`gift${colIndex}`).style.borderWidth = '4px';
@@ -79,7 +82,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.getElementById('superGift').style.backgroundColor = '#f0bd1f';
 
     document.addEventListener("keydown", async (event) => {
-        console.log("tab index :", tabIndex);
+        // console.log("tab index :", tabIndex);
         // console.log("event.key :", event);
 
 
@@ -91,13 +94,16 @@ document.addEventListener("DOMContentLoaded", async () => {
            break;
 
            case "ArrowUp" :
+            if (tabIndex == 0) {
+
+           
             if(colIndex > 0 && !isAnimating) {
                 isAnimating = true;
                 
                  colIndex --;
                  if (isElementHiddenInOverflow(document.getElementById(`gift${colIndex}`))) {
                     document.getElementById(`giftContainer`).scrollBy({
-                    top: -document.getElementById(`giftContainer`).scrollHeight / (tabIndex == 0 ? superGifts.length : vouchers.length),
+                    top: - document.getElementById(`giftContainer`).scrollHeight / (tabIndex == 0 ? superGifts.length : vouchers.length),
                     left: 0,
                     behavior:"smooth"
                   });}
@@ -115,10 +121,40 @@ document.addEventListener("DOMContentLoaded", async () => {
                   },300);
                 
             }
+          } else {
+
+            if(voucherColIndex > 0 && !isAnimating) {
+              isAnimating = true;
+              
+              voucherColIndex --;
+               if (isElementHiddenInOverflow(document.getElementById(`voucher${voucherColIndex}`))) {
+                  document.getElementById(`voucherContainer`).scrollBy({
+                  top: - document.getElementById(`voucherContainer`).scrollHeight / vouchers.length,
+                  left: 0,
+                  behavior:"smooth"
+                });}
+               
+               document.getElementById(`voucher${voucherColIndex+1}`).style.border = 'none';
+
+               document.getElementById(`voucher${voucherColIndex}`).style.borderWidth = '4px';
+               document.getElementById(`voucher${voucherColIndex}`).style.borderStyle = 'solid';
+               document.getElementById(`voucher${voucherColIndex}`).style.borderRadius = '20px';
+               document.getElementById(`voucher${voucherColIndex}`).style.borderColor = 'rgba('+10+','+ 62+','+ 235+')';
+               setTimeout(()=> {
+                    
+                  isAnimating = false;
+
+                },300);
+              
+          }
+
+          }
           
            break;
 
            case "ArrowDown" :
+
+           if(tabIndex == 0){
             if (colIndex < superGifts.length -1 && !isAnimating) {
 
                 isAnimating = true;
@@ -126,7 +162,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 colIndex ++;
                 if (isElementHiddenInOverflow(document.getElementById(`gift${colIndex}`))) {
                     document.getElementById(`giftContainer`).scrollBy({
-                    top: document.getElementById(`giftContainer`).scrollHeight / (tabIndex == 0 ? superGifts.length : vouchers.length),
+                    top: document.getElementById(`giftContainer`).scrollHeight / superGifts.length,
                     left: 0,
                     behavior:"smooth"
                   });}
@@ -148,6 +184,41 @@ document.addEventListener("DOMContentLoaded", async () => {
                 
             }
 
+           }
+           else {
+            if (voucherColIndex < vouchers.length -1 && !isAnimating) {
+
+              isAnimating = true;
+              
+              voucherColIndex++;
+
+              if (isElementHiddenInOverflow(document.getElementById(`voucher${voucherColIndex}`))) {
+                  document.getElementById(`voucherContainer`).scrollBy({
+                  top: document.getElementById(`voucherContainer`).scrollHeight / vouchers.length,
+                  left: 0,
+                  behavior:"smooth"
+                });}
+
+              document.getElementById(`voucher${voucherColIndex-1}`).style.border = 'none';
+
+              document.getElementById(`voucher${voucherColIndex}`).style.borderWidth = '4px';
+              document.getElementById(`voucher${voucherColIndex}`).style.borderStyle = 'solid';
+              document.getElementById(`voucher${voucherColIndex}`).style.borderRadius = '20px';
+              document.getElementById(`voucher${voucherColIndex}`).style.borderColor = 'rgba('+10+','+ 62+','+ 235+')';
+
+              setTimeout(()=> {
+                    
+                  isAnimating = false;
+
+                },300);
+
+
+              
+          }
+
+           }
+          
+
            break;
 
            case "ArrowRight" :
@@ -156,13 +227,17 @@ document.addEventListener("DOMContentLoaded", async () => {
                tabIndex++;
 
                document.getElementById('superGift').style.backgroundColor = 'rgba('+134+','+ 130+','+ 130+','+ 0.5+')';
-               document.getElementById('voucher').style.backgroundColor = '#f0bd1f';
+              //  rgb(10, 62, 235);
+               document.getElementById('voucher').style.backgroundColor = 'rgba('+10+','+ 62+','+ 235+')';
+               
+               giftContainer.style.display ="none";
+               voucherContainer.style.display = "block";
 
-               giftContainer.innerHTML = vouchers
-                     .map(({name,img,price,description},i) => 
+               voucherContainer.innerHTML = vouchers
+                     .map(({name,img,imgLand,price,description},i) => 
                      `
-                       <div class="giftCard" id = "gift${i}">
-                             <img src="${img}"  
+                       <div class="giftCard" id = "voucher${i}">
+                             <img src="${imgLand}"  
                              style="
                              border-radius: var(--borderRadius);
                              height: 100%;
@@ -179,10 +254,10 @@ document.addEventListener("DOMContentLoaded", async () => {
                           </div>
                         `).join("");
 
-                        document.getElementById(`gift${colIndex}`).style.borderWidth = '4px';
-                        document.getElementById(`gift${colIndex}`).style.borderStyle = 'solid';
-                        document.getElementById(`gift${colIndex}`).style.borderRadius = '20px';
-                        document.getElementById(`gift${colIndex}`).style.borderColor = '#f0bd1f';
+                        document.getElementById(`voucher${voucherColIndex}`).style.borderWidth = '4px';
+                        document.getElementById(`voucher${voucherColIndex}`).style.borderStyle = 'solid';
+                        document.getElementById(`voucher${voucherColIndex}`).style.borderRadius = '20px';
+                        document.getElementById(`voucher${voucherColIndex}`).style.borderColor = 'rgba('+10+','+ 62+','+ 235+')';
                 
 
             } 
@@ -198,6 +273,10 @@ document.addEventListener("DOMContentLoaded", async () => {
                 
                document.getElementById('superGift').style.backgroundColor = '#f0bd1f';
                document.getElementById('voucher').style.backgroundColor = 'rgba('+134+','+ 130+','+ 130+','+ 0.5+')';
+
+               giftContainer.style.display ="block";
+               voucherContainer.style.display = "none";
+
 
                giftContainer.innerHTML = superGifts
                      .map(({name,img,price,description},i) => 
@@ -219,6 +298,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 
                           </div>
                         `).join("");
+
+
+               document.getElementById(`gift${colIndex}`).style.borderWidth = '4px';
+               document.getElementById(`gift${colIndex}`).style.borderStyle = 'solid';
+               document.getElementById(`gift${colIndex}`).style.borderRadius = '20px';
+               document.getElementById(`gift${colIndex}`).style.borderColor = '#f0bd1f';
             }
                 
 
